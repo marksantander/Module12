1d29d8196ba55edc3b2bc87010e4b82398e009aa
marksantander <msntndr.ms@gmail.com>
2023-09-24 11:26:53 -0400

    added code to bd.js to use javadcript to create tables in MySQL

--- db.js
+++ db.js
@@ -6,21 +6,38 @@ const connection = mysql2.createConnection({
   user: 'root',
   password: 'root',
   database: 'db1',
-  //database: 'employee_database',
 });
 
 //module.exports = connection;
 
-connection.connect();
+const createTableQuery = `CREATE TABLE employees (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT
+  PRIMARY KEY (id)
+)`;
 
-const query = 'SELECT * FROM users';
+const createTableQuery = `CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR (30) NOT NULL,
+)`;
 
-connection.query(query, (err, results) => {
+const createTableQuery = `CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+)`;
+
+connection.query(createTableQuery, (err, results) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(results);
+    return;
   }
 
-  connection.end();
+  console.log('Table created!');
 });
+
+connection.end();
